9c3d0429adf832049a5824b4dcc7965696fa95fc
LDVerdier <ldverdier@gmail.com>
2026-03-01 10:42:53 +0100

    feat: implement favicon

--- app/root.tsx
+++ app/root.tsx
@@ -15,6 +15,7 @@ import "./app.css";
 import LanguageSwitcher from "~/components/LanguageSwitcher";
 
 export const links: Route.LinksFunction = () => [
+  { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
